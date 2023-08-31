78636443ca573b9327b5b1fc9b33819147f9b63f
Ivan Clay <ivan.clay.moura@gmail.com>
2023-08-31 08:01:17 -0300

    generate simple token

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,9 +1,29 @@
-﻿using System;
+﻿
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
+namespace JwtAspNet.Services;
 public class TokenService
 {
 	public string Create()
 	{
+		var handler = new JwtSecurityTokenHandler();
 
+		var key = Encoding.ASCII.GetBytes(Configuration.PrivateKey);
+
+		//assinar token
+		var credentials = new SigningCredentials(
+			key:new SymmetricSecurityKey(key),
+			algorithm:SecurityAlgorithms.HmacSha256);
+
+		var tokenDescription = new SecurityTokenDescriptor 
+		{
+			SigningCredentials = credentials,
+			Expires = DateTime.UtcNow.AddHours(2)
+		};
+
+		var token = handler.CreateToken(tokenDescription);
+		return handler.WriteToken(token);
 	}
 }
